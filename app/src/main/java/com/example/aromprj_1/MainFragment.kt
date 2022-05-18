ecfee3a534e6415084ed8d96b483e5c219e7cc14
Ojongseok <whdtjr6889@naver.com>
2022-05-18 16:40:51 +0900

    뷰페이저까지

--- app/src/main/java/com/example/aromprj_1/MainFragment.kt
+++ app/src/main/java/com/example/aromprj_1/MainFragment.kt
@@ -1,38 +1,38 @@
 package com.example.aromprj_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.activity_main, container, false)
 
-        viewPager.adapter=PagerAdapter(this)
-        viewPager.orientation=ViewPager2.ORIENTATION_HORIZONTAL
+
+        viewPager.adapter=PagerAdapter(FragmentActivity())
+        viewPager.orientation= ViewPager2.ORIENTATION_HORIZONTAL
         viewPager.offscreenPageLimit=3
         indicator.setViewPager(viewPager)
         indicator.createIndicators(3,0)
 
-        viewPager.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 indicator.animatePageSelected(position)
 
-                Toast.makeText(applicationContext,"${position}번 페이지 선택",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context,"${position}번 페이지 선택", Toast.LENGTH_SHORT).show()
             }
         })
 
-
     }
-    inner class PagerAdapter:FragmentStateAdapter{
+    inner class PagerAdapter: FragmentStateAdapter {
         constructor(activity: FragmentActivity):super(activity)
         override fun getItemCount() = 3
 
